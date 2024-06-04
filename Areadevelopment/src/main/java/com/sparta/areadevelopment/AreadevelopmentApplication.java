3858cb477fc073b30f3d34ace63878502429b396
Delphinium52 <63048093+Delphinium52@users.noreply.github.com>
2024-06-04 18:27:22 +0900

    feat: 첫 생성

---
+++ Areadevelopment/src/main/java/com/sparta/areadevelopment/AreadevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.areadevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreadevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AreadevelopmentApplication.class, args);
+    }
+
+}
---
+++ Areadevelopment/src/test/java/com/sparta/areadevelopment/AreadevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.areadevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreadevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
