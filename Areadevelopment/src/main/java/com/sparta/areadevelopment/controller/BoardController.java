3b016eb72543c58376508331f9fbe53232d2d1a0
hanhojin <ghwls8172@naver.com>
2024-06-10 23:40:39 +0900

    UserDetails -> CustomUserDetails 로 변경

--- Areadevelopment/src/main/java/com/sparta/areadevelopment/controller/BoardController.java
+++ Areadevelopment/src/main/java/com/sparta/areadevelopment/controller/BoardController.java
@@ -12,7 +12,6 @@ import javax.management.ServiceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
