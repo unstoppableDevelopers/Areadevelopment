7a4ce9ab39a32850cbeb400a9f5a5bf1f64ffee2
leebeanbin <wjdqlsdy1004@naver.com>
2024-06-10 13:50:57 +0900

    user crud 최종 수정
    enum 및 handler 변경사항 추가

--- Areadevelopment/src/main/java/com/sparta/areadevelopment/enums/StatusEnum.java
+++ Areadevelopment/src/main/java/com/sparta/areadevelopment/enums/StatusEnum.java
@@ -1,19 +1,6 @@
 package com.sparta.areadevelopment.enums;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum StatusEnum {
-    DELETED("DELETED"),
-    ACTIVE("ACTIVATE");
-
-    private final String stat;
-
-    StatusEnum(String stat) {
-        this.stat = stat;
-    }
-
-    @JsonValue
-    public String getStat() {
-        return stat;
-    }
+    DELETED,
+    ACTIVE;
 }
