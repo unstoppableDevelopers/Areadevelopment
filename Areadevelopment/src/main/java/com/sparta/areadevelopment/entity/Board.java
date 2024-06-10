8f8e1fbee7b277856b5865b3c5efd3a4e0272aeb
hanhojin <ghwls8172@naver.com>
2024-06-10 10:54:18 +0900

    Board 갱신, UserDetails 추가

--- Areadevelopment/src/main/java/com/sparta/areadevelopment/entity/Board.java
+++ Areadevelopment/src/main/java/com/sparta/areadevelopment/entity/Board.java
@@ -39,10 +39,10 @@ public class Board {
     private Long id;
 
     @Column(nullable = false)
-    private java.lang.String title;
+    private String title;
 
     @Column(nullable = false)
-    private java.lang.String content;
+    private String content;
 
     @Column(nullable = false)
     private Long hits;
